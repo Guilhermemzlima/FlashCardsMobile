20fb9576d5b9bb7bac2f1842484fd312978e29b7
Enzo Beltrami <enzo.beltrami@ifood.com.br>
2021-11-01 11:15:47 -0300

    Create routes

---
+++ lib/presentation/routes/routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// const homeRoute = "/home";
+// const movieDetails = "/movie_details";
+
+// final Map<String, WidgetBuilder> imoviesRoutes = {
+//   homeRoute: (BuildContext context) => HomePage(),
+//   movieDetails: (BuildContext context) => MoviePage(
+//         args: getArg<MovieDetailsArg>(context),
+//       )
+// };
+
+// T getArg<T>(BuildContext context) =>
+//     ModalRoute.of(context)!.settings.arguments as T;
